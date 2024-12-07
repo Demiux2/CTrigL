8d4885cfe2f416b29852205cedd586a7fc5b5cf8
Demiux <greatprogram7@gmail.com>
2024-12-06 21:54:48 -0300

    rad/deg unit conversion functions implementation

--- CTrigL.c
+++ CTrigL.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "CTrigL.h"
 
 #include <math.h>
@@ -14,11 +15,15 @@
 #undef ldexp
 #undef log
 #undef log10
-#undef modf
+//#undef modf
 //#undef pow
 //#undef sqrt
 #undef ceil
 //#undef fabs
+
+//
+//Trigonometric functions
+//
 
 
 //Arcocosine or inverse cosine (Arcocoseno/ACOS)
@@ -39,4 +44,33 @@ double arccos(double x){
 	double arcsin_val = asin(x);
 	double result = PI2 - arcsin_val;
 	return result;
+}
+
+double rad_to_deg(double x, bool reduce_range){
+
+    double result = x * (180.0 / PI); // rad to deg conversion
+    if (reduce_range) {
+        // Reduce to [0, 360] range
+        result = fmod(result, 360.0);
+        if (result < 0) {
+            result += 360.0; // Making the result positive
+        }
+    }
+    else{
+        return result;
+    }
+}
+
+double deg_to_rad(double deg, bool reduce_range) {
+    double result = deg * (PI / 180.0); // deg to rad conversion
+    if (reduce_range) {
+        // Reduce to [0, 2PI] range
+        result = fmod(result, 2 * PI);
+        if (result < 0) {
+            result += 2 * PI; // Making the result positive
+        }
+    }
+    else{
+        return result;
+    }
 }
--- CTrigL.h
+++ CTrigL.h
@@ -5,6 +5,9 @@
 #define PI 3.14159265358979323846
 #define PI2 1.57079632679489661923
 
+//CTrigL_trig.h
 double arccos(double x);
+double rad_to_deg(double x, bool reduce_range);
+double deg_to_rad(double x, bool reduce_range);
 
 #endif
