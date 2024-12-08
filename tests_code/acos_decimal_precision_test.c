9cb6206c7b86e74c972f2c0d3a1560f8a06c322e
Demiux <greatprogram7@gmail.com>
2024-12-08 00:18:39 -0300

    tests codes moved to \tests_code folder

---
+++ tests_code/acos_decimal_precision_test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include "..\CTrigL.h"
+
+int main(){
+
+
+	printf("arccos() is the custom implementation and acos() the math.h one\n\n");
+	printf("arccos(1.0) = %.10f\n",arccos(1.0));
+	printf("acos(1.0) = %.10f\n",acos(1.0));
+	printf("arccos(-1.0) = %.10f\n",arccos(-1.0));
+	printf("acos(-1.0) = %.10f\n",acos(-1.0));
+	printf("arccos(0.87) = %.10f\n",arccos(0.87));
+	printf("acos(0.87) = %.10f\n",acos(0.87));
+
+}
